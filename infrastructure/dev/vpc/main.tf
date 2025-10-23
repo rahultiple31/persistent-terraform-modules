974428c2fe28a3fc377f3cc06647de7e54e387ad
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2025-10-23 00:35:21 -0700

    Create main.tf

---
+++ infrastructure/dev/vpc/main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source = "git::https://github.com/Sycurio/terraform-base-module.git//aws/vpc?ref=main"
+
+  # Basic info
+  name        = var.vpc_name
+  environment = var.environment
+  network_cidr = var.vpc_network_cidr
+  zones       = var.resource_availability_zones
+
+  # Subnets
+  private_subnets_cidr = [
+    cidrsubnet(var.vpc_network_cidr, 4, 5),
+    cidrsubnet(var.vpc_network_cidr, 4, 13)
+  ]
+
+  public_subnets_cidr = [
+    cidrsubnet(var.vpc_network_cidr, 4, 7),
+    cidrsubnet(var.vpc_network_cidr, 4, 15)
+  ]
+
+  # Gateways
+  enable_nat_gateway      = var.enable_nat_gateway
+  create_internet_gateway = var.create_internet_gateway
+
+  # Common tags
+  tags = var.tags
+}
