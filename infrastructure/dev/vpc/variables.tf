7efff04ca11a9c7608dca80c69ee3134965d39da
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2025-10-23 04:14:10 -0700

    Update variables.tf

--- infrastructure/dev/vpc/variables.tf
+++ infrastructure/dev/vpc/variables.tf
@@ -23,18 +23,6 @@ variable "resource_availability_zones" {
   type        = list(string)
 }
 
-variable "transit_gateway_id" {
-  description = "Primary Transit Gateway ID used for routing."
-  type        = string
-  default     = ""
-}
-
-variable "second_transit_gateway_id" {
-  description = "Secondary Transit Gateway ID used for routing."
-  type        = string
-  default     = ""
-}
-
 variable "enable_nat_gateway" {
   description = "Enable NAT Gateway creation in VPC."
   type        = bool
@@ -45,18 +33,6 @@ variable "create_internet_gateway" {
   description = "Enable Internet Gateway creation in VPC."
   type        = bool
   default     = true
-}
-
-variable "public_subnets_cidr" {
-  description = "List of public subnet CIDR blocks."
-  type        = list(string)
-  default     = []
-}
-
-variable "private_subnets_cidr" {
-  description = "List of private subnet CIDR blocks."
-  type        = list(string)
-  default     = []
 }
 
 variable "tags" {
