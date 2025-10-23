59e80a862d380b7465dc9885e0d6c0e93591023b
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2025-10-23 03:53:45 -0700

    Update variables.tf

--- infrastructure/dev/vpc/variables.tf
+++ infrastructure/dev/vpc/variables.tf
@@ -1,50 +1,67 @@
 variable "environment" {
-  type        = string
   description = "Deployment environment name (e.g., dev, stage, prod)."
+  type        = string
 }
 
 variable "aws_region" {
+  description = "AWS region where VPC and related resources will be created."
   type        = string
-  description = "AWS region for resource deployment."
 }
 
 variable "vpc_name" {
+  description = "Name of the VPC to be created."
   type        = string
-  description = "Name of the VPC."
 }
 
 variable "vpc_network_cidr" {
-  type        = string
   description = "CIDR block for the VPC."
+  type        = string
 }
 
 variable "resource_availability_zones" {
+  description = "List of availability zones to be used for subnet creation."
   type        = list(string)
-  description = "List of availability zones for subnets."
 }
 
 variable "transit_gateway_id" {
+  description = "Primary Transit Gateway ID used for routing."
   type        = string
   default     = ""
 }
 
 variable "second_transit_gateway_id" {
+  description = "Secondary Transit Gateway ID used for routing."
   type        = string
   default     = ""
 }
 
 variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway creation in VPC."
   type        = bool
   default     = true
 }
 
 variable "create_internet_gateway" {
+  description = "Enable Internet Gateway creation in VPC."
   type        = bool
   default     = true
 }
 
+variable "public_subnets_cidr" {
+  description = "List of public subnet CIDR blocks."
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets_cidr" {
+  description = "List of private subnet CIDR blocks."
+  type        = list(string)
+  default     = []
+}
+
 variable "tags" {
-  type = map(string)
+  description = "A map of tags to assign to all resources."
+  type        = map(string)
   default = {
     Environment = ""
   }
